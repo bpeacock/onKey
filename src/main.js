85afcebfd6ebad6075081e0af710b4e3cadd09c4
Brian Peacock <bpeacock@fastfig.com>
2014-02-17 23:37:02 -0500

    Basic structure

--- src/main.js
+++ src/main.js
@@ -1,3 +1,16 @@
-module.exports = function() {
+var $ = require('jquery'),
+    Event = require('./Event.js');
 
+var key = function(selector) { //Factory for Event objects
+    return new Event(selector);
 };
+
+key.down = function(config) {
+    return new Event().down(config);
+};
+
+key.up = function(config) {
+    return new Event().up(config);
+};
+
+module.exports = key;
